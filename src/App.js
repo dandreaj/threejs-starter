cd9baee744465b3cf735f9fa662bdeccd176bb77
Jessica D'Andrea <jdandrea11@gmail.com>
2017-10-18 17:12:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Simple from './components/Simple';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="webgl-container">
+        <Simple/>
+      </div>
+    );
+  }
+}
+
+export default App;
